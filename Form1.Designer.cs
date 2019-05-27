4b51c6595df5500feacdef2193a48234ebec6ed4
Çağrı Balaban <balabancagri@gmail.com>
2019-05-27 14:19:36 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ui_to_py_program
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.donustur = new System.Windows.Forms.Button();
+            this.dosya_sec = new System.Windows.Forms.Button();
+            this.cikis = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(14, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dosya";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(82, 7);
+            this.textBox1.MaximumSize = new System.Drawing.Size(215, 35);
+            this.textBox1.MinimumSize = new System.Drawing.Size(215, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 35);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // donustur
+            // 
+            this.donustur.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.donustur.Location = new System.Drawing.Point(33, 50);
+            this.donustur.Name = "donustur";
+            this.donustur.Size = new System.Drawing.Size(75, 23);
+            this.donustur.TabIndex = 2;
+            this.donustur.Text = "Dönüştür";
+            this.donustur.UseVisualStyleBackColor = true;
+            this.donustur.Click += new System.EventHandler(this.donustur_Click);
+            // 
+            // dosya_sec
+            // 
+            this.dosya_sec.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dosya_sec.Location = new System.Drawing.Point(140, 50);
+            this.dosya_sec.Name = "dosya_sec";
+            this.dosya_sec.Size = new System.Drawing.Size(77, 23);
+            this.dosya_sec.TabIndex = 3;
+            this.dosya_sec.Text = "Dosya Seç";
+            this.dosya_sec.UseVisualStyleBackColor = true;
+            this.dosya_sec.Click += new System.EventHandler(this.dosya_sec_Click);
+            // 
+            // cikis
+            // 
+            this.cikis.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cikis.Location = new System.Drawing.Point(249, 50);
+            this.cikis.Name = "cikis";
+            this.cikis.Size = new System.Drawing.Size(75, 23);
+            this.cikis.TabIndex = 4;
+            this.cikis.Text = "Exit";
+            this.cikis.UseVisualStyleBackColor = true;
+            this.cikis.Click += new System.EventHandler(this.cikis_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(7, 78);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(328, 96);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(14, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Giriniz";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(14, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Adı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 182);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.cikis);
+            this.Controls.Add(this.dosya_sec);
+            this.Controls.Add(this.donustur);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(359, 39);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UI TO PY";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button donustur;
+        private System.Windows.Forms.Button dosya_sec;
+        private System.Windows.Forms.Button cikis;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ui_to_py_program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            donustur.Enabled = false;
+            richTextBox1.Text = "Lütfet UI dosyanızı Seçininiz\nLütfen Dosyanızı seçmeden önce PY dosyanızın adını belirleyiniz";
+            
+        }
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        string dosyaadi;
+        string dosyayolu;
+        string yeniad;
+        string[] yol;
+        string yeniyol = "";
+        string cmdkomutu;
+        string dosya;
+        
+        private void dosya_sec_Click(object sender, EventArgs e)
+        {
+        
+            OpenFileDialog dosya = new OpenFileDialog();
+            
+            dosya.Filter = "Ui Dosyası |*.ui";
+            dosya.RestoreDirectory = true;
+            if (dosya.ShowDialog() == DialogResult.OK)
+            {
+                
+                dosyaadi = dosya.SafeFileName;
+                dosyayolu = dosya.FileName;
+            }
+            if ((dosyaadi == "" || dosyaadi == null) || (dosyayolu == "" || dosyayolu == null))
+            {
+                MessageBox.Show("Dosya Seçmediniz\nLütfen Dosyanızı Tekrar Seçiniz");
+                              
+
+            }
+            else
+            {
+                                               
+                yeniad = textBox1.Text;
+                if (yeniad == "")
+                {
+                    MessageBox.Show("Çıkarılacak Dosya Adını belirleyiniz Ve Dosyanızı Baştan Seçiniz");
+                    
+                }
+                else
+                {
+                    yeniyol = "";
+                    yol = dosyayolu.Split('\\');
+                    Array.Resize(ref yol, yol.Length - 1);
+
+                    for (int i = 0; i < yol.Length; i++)
+                    {
+                        yeniyol += yol[i].ToString() + "/";
+                    }
+                    richTextBox1.Text = "Dosya Adı:" + dosyaadi + "\n" + "Dosya Yolu:" + yeniyol + "\n" + "Çıkacak Dosya Adı:" + yeniad;
+
+                    donustur.Enabled = true;
+                }
+            }
+        }
+
+        private void cikis_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void donustur_Click(object sender, EventArgs e)
+        {
+            if ((yeniyol == "" || yeniyol == null) || (yeniad == "" || yeniad == null))
+            {
+                
+                MessageBox.Show("Dosya Seçilmedi" +"\n"+ "Lütfen Dosyayı Seçip Tekrar Deneyiniz");
+                return;
+
+             }
+            else
+            {
+                cmdkomutu = "/C pyuic5 " + dosyaadi + " -o " + yeniad + ".py";
+                System.Diagnostics.Process x = new System.Diagnostics.Process();
+                System.Environment.CurrentDirectory = @yeniyol;
+                System.Diagnostics.Process.Start("cmd.exe", cmdkomutu);
+                System.Diagnostics.Process.Start(@yeniyol);
+                donustur.Enabled = false;
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
